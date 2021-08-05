63b7a30cf753d0f5085d254916629ed2b5b4e227
Nguyễn Hải Anh <haianh30198@gmail.com>
2021-08-06 00:01:26 +0700

    Bai 70 | Tich Hop Thu Vien

--- src/App.js
+++ src/App.js
@@ -1,25 +1,65 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import Home from "./components/Home";
+import About from "./components/About";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+    render() {
+        return (
+            <Router>
+                <div className="App">
+                    {/* Menu */}
+                    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+                        <a className="navbar-brand" href="/#">
+                            <img
+                                src="https://getbootstrap.com/docs/4.1/assets/brand/bootstrap-solid.svg"
+                                width="30"
+                                height="30"
+                                className="d-inline-block align-top"
+                                alt=""
+                            />
+                            Bootstrap
+                        </a>
+                        <button
+                            className="navbar-toggler"
+                            type="button"
+                            data-toggle="collapse"
+                            data-target="#navbarNav"
+                            aria-controls="navbarNav"
+                            aria-expanded="false"
+                            aria-label="Toggle navigation"
+                        >
+                            <span className="navbar-toggler-icon"></span>
+                        </button>
+                        <div
+                            className="collapse navbar-collapse"
+                            id="navbarNav"
+                        >
+                            <ul className="navbar-nav">
+                                <li className="nav-item active">
+                                    <a className="nav-link" href="/#">
+                                        Home{" "}
+                                        <span className="sr-only">
+                                            (current)
+                                        </span>
+                                    </a>
+                                </li>
+                                <li className="nav-item">
+                                    <a className="nav-link" href="/#">
+                                        Features
+                                    </a>
+                                </li>
+                            </ul>
+                        </div>
+                    </nav>
+                    {/* Content */}
+                    <Route path="/" exact component={Home} />
+                    <Route path="/about" component={About} />
+                </div>
+            </Router>
+        );
+    }
 }
 
 export default App;
