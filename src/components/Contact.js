c69de85fe4f0941b101939fd5405695234803939
Nguyễn Hải Anh <haianh30198@gmail.com>
2021-08-14 18:41:13 +0700

    Bai 78 | Doi Tuong Prompt

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,8 +1,46 @@
 import React, { Component } from "react";
+import { Prompt } from "react-router-dom";
 
 class Contact extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            isChecked: false,
+        };
+    }
+
+    onClick = (isChecked) => {
+        this.setState({ isChecked });
+    };
+
     render() {
-        return <div>This is Contact page</div>;
+        var { isChecked } = this.state;
+        return (
+            <div>
+                This is Contact page
+                <br />
+                <button
+                    type="button"
+                    className="btn btn-success"
+                    onClick={() => this.onClick(false)}
+                >
+                    Enable
+                </button>
+                <button
+                    type="button"
+                    className="btn btn-danger"
+                    onClick={() => this.onClick(true)}
+                >
+                    Disable
+                </button>
+                <Prompt
+                    when={isChecked}
+                    message={(location) =>
+                        `Bạn chắc muốn đi tên trang ${location.pathname}`
+                    }
+                />
+            </div>
+        );
     }
 }
 
